b2986374474f969e6d025a438258932103ceb3d8
M.Shahzaib <mohdshahzaib24@gmail.com>
2020-11-21 02:21:01 +0500

    app completed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import Timer from "./views/Timer/Timer";
+
+describe("App", () => {
+  let container: any;
+
+  beforeEach(() => (container = shallow(<App />)));
+
+  it("should render 1 div", () => {
+    expect(container.find("div").length).toEqual(1);
+  });
+
+  it("should render 1 span", () => {
+    expect(container.find("span").length).toEqual(1);
+  });
+
+  it("should render the Timer Component", () => {
+    expect(container.containsMatchingElement(<Timer />)).toEqual(true);
+  });
 });
